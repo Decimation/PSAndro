2ceb32e0e9bee3b34e24d43f24231a5715f86f39
Read Stanton <decimation001@gmail.com>
2024-10-02 17:47:12 -0500

    2024-10-02 17:47:12

--- Android.psm1
+++ Android.psm1
@@ -456,14 +456,26 @@ function Adb-Stat {
 	$x = Adb-Escape -x $x -e Shell
 	
 	$d = "   "
-	$a = "%n", "%N", "%F", "%w", "%x", "%y", "%z", "%s" -join $d
+
+	$a = "%n", `
+		"%N", `
+		"%F", `
+		"%w", `
+		"%x", `
+		"%y", `
+		"%z", `
+		"%s" `
+		-join $d
+
 	$cmd = @("stat -c '$a' $x")
 	$out = [string] (Invoke-AdbShell @cmd)
 	$rg = $out -split $d
 	$i = 0
 
+	$origName = $rg[$i++] -split '/' | Select-Object -Last 1
+
 	$obj = [PSCustomObject]@{
-		Name             = $rg[$i++]
+		Name             = $origName
 		FullName         = $rg[$i++]
 		Type             = $rg[$i++]
 		TimeOfBirth      = $rg[$i++]
@@ -484,9 +496,47 @@ function Adb-Stat {
 	
 	<# $cmd = @('shell', "stat $x")
 	$out = [string] (adb @cmd)
-	$rg = $out -split "`n" | ForEach-Object { $_.Trim() } #>
-
+	$rg = $out -split "`n" | ForEach-Object { $_.Trim() } 
+	#>
 	
+
+	# $statArgs = '%n %F %w %x %y %z %s'
+	# $cmd = "for file in *; do stat -c $statArgs `$file; done"
+	
+	<#region
+	
+	Toybox 0.8.9-android multicall binary (see toybox --help)
+
+	usage: stat [-tfL] [-c FORMAT] FILE...
+
+	Display status of files or filesystems.
+
+	-c      Output specified FORMAT string instead of default
+	-f      Display filesystem status instead of file status
+	-L      Follow symlinks
+	-t      terse (-c "%n %s %b %f %u %g %D %i %h %t %T %X %Y %Z %o")
+				(with -f = -c "%n %i %l %t %s %S %b %f %a %c %d")
+
+	The valid format escape sequences for files:
+	%a  Access bits (octal) |%A  Access bits (flags)|%b  Size/512
+	%B  Bytes per %b (512)  |%C  Security context   |%d  Device ID (dec)
+	%D  Device ID (hex)     |%f  All mode bits (hex)|%F  File type
+	%g  Group ID            |%G  Group name         |%h  Hard links
+	%i  Inode               |%m  Mount point        |%n  Filename
+	%N  Long filename       |%o  I/O block size     |%s  Size (bytes)
+	%t  Devtype major (hex) |%T  Devtype minor (hex)|%u  User ID
+	%U  User name           |%x  Access time        |%X  Access unix time
+	%y  Modification time   |%Y  Mod unix time      |%z  Creation time
+	%Z  Creation unix time
+
+	The valid format escape sequences for filesystems:
+	%a  Available blocks    |%b  Total blocks       |%c  Total inodes
+	%d  Free inodes         |%f  Free blocks        |%i  File system ID
+	%l  Max filename length |%n  File name          |%s  Best transfer size
+	%S  Actual block size   |%t  FS type (hex)      |%T  FS type (driver name)
+
+	#>
+
 	return $obj
 }
 
